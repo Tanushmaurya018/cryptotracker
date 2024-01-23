160bd81a7abfafdd95c43c71270ca17bdebd85cc
Tanush Maurya <tanushmaurya018@gmail.com>
2024-01-24 01:39:33 +0530

    made trending section responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ const Card = (props) => {
 
   return (
     <div className=" hover:scale-105  duration-200 bg-gray-700 text-white bg-opacity-40 backdrop-blur hover:bg-white transition-all ease-linear hover:text-black m-5 rounded-2xl p-4">
-      <div className="flex justify-between w-[300px] md:w-[300px] h-[100px]  gap-5 overflow-hidden b">
+      <div className="flex flex-col md:flex-row justify-between w-[100px] md:w-[300px] h-[200px] md:h-[100px]  gap-5 overflow-hidden b">
         <img src={`${props.imageUrls}`} className=" object-cover rounded-2xl flex justify-center items-center w-[100px] h-[100px]" />
-        <div className="justify-between items-center text-right">
-        <h1 className="text-2xl font-bold ">{props.name}</h1>
-        <h1 className="text-xl  font-medium ">
+        <div className="justify-between items-center text-center md:text-right">
+        <h1 className="text-md md:text-2xl font-bold ">{props.name}</h1>
+        <h1 className="text-md md:text-xl  font-medium ">
         {props.price}</h1>
         </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,36 +27,33 @@ const Home = () => {
 
   const fetchTrendingData = async () => {
     try {
-      setLoading(true)
+      setLoading(true);
       const res = await axios.get(
         `https://api.coingecko.com/api/v3/search/trending`
       );
       setTrendingCrypto(res.data.coins);
-      setLoading(false)
-
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  function counter(){
-    let count =0 ;
-    count ++;
-    return count
+  function counter() {
+    let count = 0;
+    count++;
+    return count;
   }
   const fetchCoinData = async () => {
-    const counter=0;
+    const counter = 0;
     try {
-      setLoading(true)
+      setLoading(true);
 
       const res = await axios.get(`
       https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en\
-      `
-      )
-      console.log(res.data)
+      `);
+      console.log(res.data);
       setAllCoins(res.data);
-      setLoading(false)
-
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -130,19 +127,19 @@ const Home = () => {
           <span>Trending</span>
           <IoIosTrendingUp />
         </div>
-      {
-        loading ? <Loader/> :
-        <div className=" flex flex-wrap justify-center items-center ">
-          {trendingCrypto?.map((coin) => (
-            <Card
-              imageUrls={coin.item.large}
-              name={coin.item.name}
-              price={coin.item.data.price}
-            />
-          ))}
-        </div>
-      }
-
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className=" flex flex-wrap justify-center items-center ">
+            {trendingCrypto?.map((coin) => (
+              <Card
+                imageUrls={coin.item.large}
+                name={coin.item.name}
+                price={coin.item.data.price}
+              />
+            ))}
+          </div>
+        )}
       </div>
       <div className="flex flex-col">
         <div
@@ -152,55 +149,57 @@ const Home = () => {
           <span>Crypto's</span>
           <CiBitcoin />
         </div>
-      {
-        loading ? <Loader/> :
-        <div className=" flex  justify-center items-center ">
-          <div className="overflow-x-scroll w-full">
-            <table className="w-full divide-y divide-gray-200 bg-gray-900 bg-opacity-30 backdrop-blur text-gray-300">
-              <thead>
-                <tr className="">
-                <th className="py-2 px-4">S.No</th>
-                <th className="py-2 px-4">Image</th>
-                  <th className="py-2 px-4">Name</th>
-                  <th className="py-2 px-4">Price</th>
-                  <th className="py-2 px-4">Market Cap</th>
-                  <th className="py-2 px-4">Market Cap Change (24h)</th>
-                </tr>
-              </thead>
-              <tbody>
-                {allCoins?.map((coin,index) => (
-                  <tr className=" hover:bg-gray-100 text-center">
-                    <td className="py-2 px-4">{index +1}</td>
-                    <td className="py-2 px-4 w-[30px] md:w-[50px] h-[30px] md:h-[50px]">
-                      <img src={coin.image} />
-                    </td>
-                    <td className="py-2 px-4">{}</td>
-                    <td className="py-2 px-4">
-                      ${coin.current_price.toLocaleString()}
-                    </td>
-                    <td className="py-2 px-4">
-                      $
-                      {coin.market_cap.toLocaleString(undefined, {
-                        minimumFractionDigits: 0,
-                        maximumFractionDigits: 2,
-                        notation: "compact",
-                        compactDisplay: "short",
-                      })}
-                    </td>
-                    <td className="py-2 px-4">{coin.market_cap_change_24h.toLocaleString(undefined, {
-                        minimumFractionDigits: 0,
-                        maximumFractionDigits: 2,
-                        notation: "compact",
-                        compactDisplay: "short",
-                      })}</td>
+        {loading ? (
+          <Loader />
+        ) : (
+          <div className=" flex  justify-center items-center ">
+            <div className="overflow-x-scroll w-full">
+              <table className="w-full divide-y divide-gray-200 bg-gray-900 bg-opacity-30 backdrop-blur text-gray-300">
+                <thead>
+                  <tr className="">
+                    <th className="py-2 px-4">S.No</th>
+                    <th className="py-2 px-4">Image</th>
+                    <th className="py-2 px-4">Name</th>
+                    <th className="py-2 px-4">Price</th>
+                    <th className="py-2 px-4">Market Cap</th>
+                    <th className="py-2 px-4">Market Cap Change (24h)</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {allCoins?.map((coin, index) => (
+                    <tr className=" hover:bg-gray-500 hover:scale-105 transition-all ease-in-out text-center">
+                      <td className="py-2 px-4">{index + 1}</td>
+                      <td className="py-2 px-4 w-[30px] md:w-[50px] h-[30px] md:h-[50px]">
+                        <img src={coin.image} />
+                      </td>
+                      <td className="py-2 px-4">{coin.name}</td>
+                      <td className="py-2 px-4">
+                        ${coin.current_price.toLocaleString()}
+                      </td>
+                      <td className="py-2 px-4">
+                        $
+                        {coin.market_cap.toLocaleString(undefined, {
+                          minimumFractionDigits: 0,
+                          maximumFractionDigits: 2,
+                          notation: "compact",
+                          compactDisplay: "short",
+                        })}
+                      </td>
+                      <td className="py-2 px-4">
+                        {coin.market_cap_change_24h.toLocaleString(undefined, {
+                          minimumFractionDigits: 0,
+                          maximumFractionDigits: 2,
+                          notation: "compact",
+                          compactDisplay: "short",
+                        })}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      }
-
+        )}
       </div>
     </div>
   );
